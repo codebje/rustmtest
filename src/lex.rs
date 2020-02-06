4e1673ea117e625357fe886d181722010dedd54e
Byron Ellacott <code@bje.id.au>
2020-02-07 08:19:45 +1000

    cribbing more from cloudflare

--- src/lex.rs
+++ src/lex.rs
@@ -3,20 +3,32 @@
  *
  */
 
-pub enum ParseError {
+pub enum LexErrorKind {
     Ouch(),
 }
 
+pub type LexError<'i> = (LexErrorKind, &'i str);
+
+pub type LexResult<'i, T> = Result<(T, &'i str), LexError<'i>>;
+
 pub trait Lex<'i>: Sized {
-    fn lex(input: &'i str) -> Result<(Self, &'i str), ParseError>;
+    fn lex(input: &'i str) -> LexResult<'i, Self>;
 }
 
 pub trait LexWith<'i, C>: Sized {
-    fn lex(input: &'i str, ctx: C) -> Result<(Self, &'i str), ParseError>;
+    fn lex(input: &'i str, ctx: C) -> LexResult<'i, Self>;
 }
 
 impl<'i, T: Lex<'i>, C> LexWith<'i, C> for T {
-    fn lex(input: &'i str, _ctx: C) -> Result<(Self, &'i str), ParseError> {
+    fn lex(input: &'i str, _ctx: C) -> LexResult<'i, Self> {
         Self::lex(input)
+    }
+}
+
+pub fn expect<'i>(input: &'i str, s: &'static str) -> Result<&'i str, LexError<'i>> {
+    if input.starts_with(s) {
+        Ok(&input[s.len()..])
+    } else {
+        Err((LexErrorKind::Ouch(), input))
     }
 }
